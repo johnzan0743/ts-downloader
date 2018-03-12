b434e7448799de947c7fe337211f6b968fbf87a2
Samer Albahra <salbahra@gmail.com>
2018-03-12 16:50:27 -0500

    Move the URL to download as a parameter
    This allows the code to be used without having to edit the source each time.

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
-# TSdownloader
-## download segmented mpeg video (.m3u8/.ts) from streaming websites
+# TS Downloader
+## Download segmented mpeg video (.m3u8/.ts) from streaming websites
 1. Make sure you have "requests" installed:
-```    
+```sh
        pip install requests
-       pip install -U requests[security] 
+       pip install -U requests[security]
 ```
 
-2. Specify the urls in the .py file
-
-3. Run:
-```    python tsdownload.py destination ```
-
+2. Run:
+```sh
+python tsdownload.py m3u8-url destination
+```
--- tsdownload.py
+++ tsdownload.py
@@ -3,9 +3,7 @@ import sys
 import os
 import requests
 
-M3URL = ""  # fill in: .m3u8 file url
-SEG1URL = ""  # fill in: first ts segment file url
-REFERER = ""  # fill in: some websites require a specific referer website to allow the request
+REFERER = ""
 
 OUTNAME = 'video.ts'  # default output file name
 LOC = ""  # default save location
@@ -23,29 +21,23 @@ HEADERS = {
 }
 
 
-def getSegsNum(m3):
+def getSegs(m3):
     """ figure out how many segments there are using the m3u8 file """
     lines = m3.text.split('\n')
-    nsegs = 0
+    segments = []
     for line in lines:
-        if 'seg-' in line and '.ts' in line:
-            tokens = line.split('-')
-            idx = 0
-            for i, tok in enumerate(tokens):
-                if 'seg' == tok[-3:]:
-                    idx = i + 1
-                    break
-            nsegs = int(tokens[idx])
-    return nsegs
+        if '.ts' in line:
+            segments.append(line)
+    return segments
 
 
-def dumpSegs(initUrl, n, path, append=False):
+def dumpSegs(initUrl, segments, path, append=False):
     """ downlaod and combine the .ts files
     given the first seg's url, the number of segments and
     the destination download path """
     with open(path, 'ab' if append else 'wb') as f:
-        for i in range(1, n + 1):
-            segurl = initUrl.replace('seg-1-', 'seg-{:d}-'.format(i))
+        for segment in segments:
+            segurl = initUrl + '/' + segment
             success = False
             while not success:
                 try:
@@ -54,13 +46,13 @@ def dumpSegs(initUrl, n, path, append=False):
                 except:
                     print('retrying...')
             f.write(seg.content)
-            print(('dumped seg%d.ts' % i) + '  %d%%' % (i * 100 / n))
 
 
 if __name__ == "__main__":
     DEST = LOC + OUTNAME
-    if len(sys.argv) > 1:
-        DEST = sys.argv[1]
+    SOURCE = sys.argv[1]
+    if len(sys.argv) > 2:
+        DEST = sys.argv[2]
     # validate destination:
     delim = ''
     if '\\' in DEST:
@@ -72,6 +64,7 @@ if __name__ == "__main__":
         if not os.path.isdir(PATH):
             print('INAVLID DESTINATION.')
             sys.exit(0)
-    m3u8 = requests.get(M3URL, headers=HEADERS)
-    nsegs = getSegsNum(m3u8)
-    dumpSegs(SEG1URL, nsegs, DEST)
+    m3u8 = requests.get(SOURCE, headers=HEADERS)
+    segments = getSegs(m3u8)
+    url = '/'.join(SOURCE.split('/')[:-1])
+    dumpSegs(url, segments, DEST)
